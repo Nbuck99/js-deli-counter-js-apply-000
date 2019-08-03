fc72707dca3e78471326773ce32c6f4abe99d898
Nathan Buchanan <nate.buchanan@me.com>
2019-08-03 22:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ for(var i=0; i < katzDeliLine.length ; i++){
   line += (i+1) + ". " + katzDeliLine[i] + ", ";
 }
 line = line.slice(0, line.length-2)
-return "The line is currently "+line;
+return "The line is currently: "+line;
 }
 }
