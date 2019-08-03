e8733cb7ee7c8cf4e5af579e07cf6933d97a8487
Nathan Buchanan <nate.buchanan@me.com>
2019-08-03 22:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ katzDeliLine.splice(0,1)
 return "Currently serving "+firstname+".";
 }
 }
+
+function currentLine(katzDeliLine){
+var line =[];
+if (katzDeliLine.length == 0) {
+return "The line is currently empty."
+}
+else{
+for(var i=0; i < katzDeliLine.length ; i++){
+  line += (i+1) + ". " + katzDeliLine[i] + ", ";
+}
+line = line.slice(0, line.length-2)
+return "The line is currently "+line;
+}
+}
