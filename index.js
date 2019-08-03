f9b65e2a01ca8a86a90543975cf025106a3feafe
Nathan Buchanan <nate.buchanan@me.com>
2019-08-03 21:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function takeANumber(katzDeliLine,name){
 katzDeliLine.push(name);
 return "Welcome, "+name+". You are number "+katzDeliLine.length+" in line.";
 }
+
+function nowServing(katzDeliLine){
+if (katzDeliLine.length == 0) {
+  "There is nobody waiting to be served!"  
+}
+else{
+var firstname = katzDeliLine[0]
+katzDeliLine.splice(0,1) 
+return "Currently serving "+firstname; 
+}
+}
